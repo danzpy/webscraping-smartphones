f2bb9a1adb9d9fdb8f5c767afcb67ef2a7d2b1ed
Dan <daniel.leonssio@hotmail.com>
2024-09-03 18:35:14 -0300

    Feature: Inclusão da coluna 'armazenamento'

--- transform/transform.py
+++ transform/transform.py
@@ -12,15 +12,39 @@ dados = pd.read_csv("data/informacoes-smartphones.csv", encoding="utf-8", sep=";
 # Utilizar metodo .extract para coletar informação e adiciona-la em uma nova coluna.
 
 
-def memoria_ram(df):
+def memoria_ram(df) -> pd.DataFrame:
 
     # O padrão abaixo foi encontrado com auxílio de inteligência artificial.
     pattern = r"\b(1[0-2]|[1-9])gb\b"
 
-    df["ram"] = df["titulo"].str.extract(pattern, flags=re.IGNORECASE)
+    df["ram (gb)"] = df["titulo"].str.extract(pattern, flags=re.IGNORECASE)
 
     return df
 
 
-ram = memoria_ram(dados)
-ram.to_csv("data/teste.csv", index=False, sep=";", encoding="utf-8")
+def armazenamento(df) -> pd.DataFrame:
+
+    # O padrões abaixo foram encontrados com auxílio de inteligência artificial.
+    # pattern_gb = r"\b([1-9]\d{1,2}|1[6-9])\s*[gG][bB]\b"
+    pattern_gb = r"\b(1[6-9]|[2-9]\d|\d{3})\s*[gG][bB]\b"
+    pattern_tb = r"\b1\s*[tT][bB]\b"
+
+    df["armazenamento (gb)"] = df["titulo"].str.extract(pattern_gb, flags=re.IGNORECASE)
+
+    df.loc[
+        df["titulo"].str.contains(pattern_tb, flags=re.IGNORECASE), "armazenamento (gb)"
+    ] = "1024"
+
+    return df
+
+
+def transform(df) -> pd.DataFrame:
+
+    df = memoria_ram(df)
+    df = armazenamento(df)
+
+    return df
+
+
+df = transform(dados)
+df.to_csv("data/teste.csv", index=False, sep=";", encoding="utf-8")
